9bfed6da75b8b25e6b8335d18e8268f772ad45ba
Magnus Kühne <kuehne.magnus@gmail.com>
2026-03-28 02:48:35 +0100

    ci(e2e): fix e2e tests

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: 1,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? [["github"], ["html"]] : "html",
   timeout: 60_000,
   expect: {
     timeout: 10_000,
